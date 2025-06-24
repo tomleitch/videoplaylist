157a5dda26b6cdf89cfc7b63967034e1a067e622
tomleitch <36605802+tomleitch@users.noreply.github.com>
2025-06-24 19:16:41 -0300

    Codebase
    Codebase

---
+++ SQL Scripts/ActorActorTag.sql
@@ -0,0 +1,20 @@
+
+/****** Object:  Table [dbo].[ActorActorTag]    Script Date: 24/06/2025 19:14:24 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[ActorActorTag](
+	[ActorId] [int] NOT NULL,
+	[ActorTagId] [int] NOT NULL,
+ CONSTRAINT [PK_ActorActorTag] PRIMARY KEY CLUSTERED 
+(
+	[ActorId] ASC,
+	[ActorTagId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+
